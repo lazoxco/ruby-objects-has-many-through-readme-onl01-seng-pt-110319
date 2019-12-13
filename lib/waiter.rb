629095ec91a957c63e9c208054816ae1d1206675
Nory Lazo <nlazo@icloud.com>
2019-12-13 09:31:03 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -16,4 +16,8 @@ class Waiter
   def new_meal(customer, total, tip)
     Meal.new(self, customer, total, tip)
   end 
+  
+  def meals
+    Meal.all.select { |meal| meal.waiter == self}
+  end 
 end
