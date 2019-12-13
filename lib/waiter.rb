56b680aaf58c967390894f6c933f9890743e14e1
Nory Lazo <nlazo@icloud.com>
2019-12-13 09:21:14 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,8 +1,14 @@
 class Waiter
   attr_accessor :name, :experience
   
+  @@all = []
+  
   def initialize(name, experience)
     @name = name
     @experience = experience
+  end 
+  
+  def self.all 
+    @@all
   end 
 end
